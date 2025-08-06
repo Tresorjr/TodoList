be8d5b00c9212ca7d2ffe54b34a218e7ec3e1486
Tresorjr <ngassomtresor7@gmail.com>
2025-08-06 10:20:58 -0700

    hello updatehello updatehello updatesss

--- TodoList/manage_users/urls.py
+++ TodoList/manage_users/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('main/', views.main, name='main'),
     path('', views.index, name='index'),
 ]
--- TodoList/manage_users/views.py
+++ TodoList/manage_users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'manage_users/index.html')
+
+def main(request):
+    return render(request, 'manage_users/mainhtml.html')
